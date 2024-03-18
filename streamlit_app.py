ee67b8e1c40b3ff313e8a7348af59c1731eb7c8f
Vishal Anand Chenna <vishalanandch@gmail.com>
2024-03-17 21:48:04 -0500

    Update streamlit_app.py
    Adding another column to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop
 
